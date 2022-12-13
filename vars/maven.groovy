6bd854bcc8aa3cd074576ad8f4939fbbb01f1db1
swetha chowdary <swethachowdary2693@gmail.com>
2022-12-13 20:49:37 +0530

    lint checks

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -2,7 +2,7 @@ def lintcheck() {
     sh ''' 
         # We want Developers to handle the lint checks failure
         echo Starting lint checks for ${Component}
-        # mvn checkstyle:check || true                     //perform maven lint checks
+        mvn checkstyle:check || true                     //perform maven lint checks
         echo Lint checks completed for ${Component}
     '''
 }
