3b759ed64cd242d03ae689c0f8c99ad9e15e3c0b
swetha chowdary <swethachowdary2693@gmail.com>
2022-12-13 20:52:09 +0530

    lint checks

---
+++ vars/python.groovy
@@ -0,0 +1,25 @@
+def lintcheck() {
+    sh ''' 
+        # We want Developers to handle the lint checks failure
+        echo Starting lint checks for ${Component}
+        # pylint *.py                     //perform python lint checks
+        echo Lint checks completed for ${Component}
+    '''
+}
+
+def call() {
+    pipeline {
+    agent any 
+    stages {
+        
+        stage ('Lint Checks') {
+            steps {
+                script {
+                    lintcheck()
+                }
+                
+            }
+        }
+    }   //end of the stages
+}
+}
